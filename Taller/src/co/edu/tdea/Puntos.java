25e13a5132775fd13af6b06d11aa40d3ebee997c
Alex <alexius9000@gmail.com>
2022-02-25 00:01:21 -0500

    FINISH

--- Taller/src/co/edu/tdea/Puntos.java
+++ Taller/src/co/edu/tdea/Puntos.java
@@ -1,22 +1,22 @@
 package co.edu.tdea;
 
-import java.util.Iterator;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Puntos {
 	public void p1_promedio(Scanner sc) {
-		int nota1, nota2, nota3;
+		double nota1, nota2, nota3;
 		
 		System.out.println("Digite la primera nota");
-		nota1 = sc.nextInt();
+		nota1 = sc.nextDouble();
 		System.out.println("Digite la segunda nota");
-		nota2 = sc.nextInt();
+		nota2 = sc.nextDouble();
 		System.out.println("Digite la tercera nota");
-		nota3 = sc.nextInt();
+		nota3 = sc.nextDouble();
 		
 		double promedio = (nota1*0.30) + (nota2*0.30) + (nota3 * 0.40);
 		
-		System.out.println("El promedio de la nota es" + promedio);
+		System.out.println("El promedio de la nota es: " + promedio);
 	}
 	
 	public void p2_perimetro_cuadrado(Scanner sc) {
@@ -32,15 +32,14 @@ public class Puntos {
 		int area = base*altura,
 			perimetro = (2*base)+(2*altura);
 		
-		System.out.println("area" + area);
-		System.out.println("perimetro" + perimetro);
+		System.out.println("area: " + area);
+		System.out.println("perimetro: " + perimetro);
 		
 	}
 	
 	public void p3() {
-		System.out.println("...");
+			System.out.println("...");
 	}
-	
 	
 	public void p4_perimetro_triangulo(Scanner sc) {
 		int ladoA, ladoB, ladoC;
@@ -53,12 +52,14 @@ public class Puntos {
 		ladoC = sc.nextInt();
 		
 		int perimetro = (ladoA + ladoB + ladoC),
-				semi = perimetro / 2,
-				area = (semi * (semi - ladoA)*(semi - ladoB) *(semi - ladoC));
+				semi = (int) ((ladoA+ladoB+ladoC)/2);
+				Double area = Math.sqrt(semi* (semi - ladoA) * (semi - ladoB) * (semi - ladoC));
+				
 		
-		System.out.println("perimetro:"+ perimetro);
-		System.out.println("semi-perimetro:"+ semi);
-		System.out.println("area:"+ area);
+		
+		System.out.println("perimetro: "+ perimetro);
+		System.out.println("semi-perimetro: "+ semi);
+		System.out.println("area: "+ area);
 	}
 	
 	public void p5_divisores(Scanner sc) {
@@ -80,7 +81,6 @@ public class Puntos {
 			
 			
 			while (j < nro) {
-				
 				sumaTotal = sumaTotal + j;
 				
 				if (sumaTotal >= nro) {
@@ -95,7 +95,104 @@ public class Puntos {
 				}
 				j++;
 			}
+			System.out.print("= "+ nro);
+		} else {
+			System.out.println(" El nro no es primo");
 		}
 		
 	}
+
+	public void p6_acumulado(Scanner sc) {
+		int acumulado = 0;
+		double total;
+
+		for (int i = 0; i < 99; i++) {
+			acumulado += i;
+		}
+		
+		total = Math.sqrt(acumulado);
+		System.out.println("Acumulado:" + total);
+	}
+	
+	public void p7_logaritmo() {
+		int sumTotal = 0;
+		double total;
+		
+		for (int i = 0; i < 199; i++) {
+			sumTotal += i*i;
+		}
+		
+		total = Math.log(sumTotal);
+		
+		System.out.println("Logaritmo: " + total);
+	}
+	
+	public void p8_time() {
+		LocalTime now = LocalTime.now();
+		LocalTime horaInicial = now.with(LocalTime.MIDNIGHT);
+		
+		int diff = (now.getHour() - horaInicial.getHour());
+		
+		System.out.println("La cantidad de hora que pasaron desde la 12:00am es: " + diff);
+	}
+	
+	public void p9_suma_cubos(Scanner sc) {
+		int n, sumTotal = 0;
+		
+		System.out.println("ingrese n");
+		n = sc.nextInt();
+		
+		for (int i = 0; i <= n; i++) {
+			if (i % 2 == 0) {
+				sumTotal += Math.pow(i, 3);
+			}
+		}
+		
+		System.out.println(
+			"La suma total de los cubos para el nro" + " " +
+			n + " " + "es: " + sumTotal
+		);
+	}
+	
+	public void p10_productos(Scanner sc, Boolean isRepeatEjercicio) {
+			String nombreProducto;
+			double valorProducto, descuento, total, neto, valorImpuesto;
+			int impuesto;
+			System.out.println("Digite el nombre del producto");
+			nombreProducto = sc.next();
+			
+			System.out.println("Digite el valor del producto");
+			valorProducto = sc.nextDouble();
+			
+			System.out.println("Digite el valor del impuesto (en entero)");
+			impuesto = sc.nextInt();
+		
+			System.out.println("producto:" + nombreProducto);
+
+
+			double porcentaje = Double.parseDouble("0." + impuesto);
+			if (impuesto >= 10 && impuesto <= 20) {
+				valorImpuesto = porcentaje * valorProducto;
+				neto = valorImpuesto + valorProducto;
+				total = ((neto * 0.40)+ neto);
+				
+				System.out.println("valorImpuesto:" + valorImpuesto);
+				System.out.println("neto:" + neto);
+				System.out.println("total:" + total);
+			}else if (impuesto > 20) {
+				valorImpuesto = porcentaje * valorProducto;
+				double porcentajeDescuento = Double.parseDouble("1.5") / 100;
+
+				descuento =  (long) (valorProducto * porcentajeDescuento);
+				total = (valorImpuesto + valorProducto) - descuento;
+
+				System.out.println("valorImpuesto:"+ valorImpuesto);
+				System.out.println("descuento:"+ descuento);
+				System.out.println("total:"+ total);
+			} else {
+				System.out.println("porcentaje fuera de rango:" + impuesto);
+			}
+	}
+	
+	
 }
--- Taller/src/co/edu/tdea/Taller.java
+++ Taller/src/co/edu/tdea/Taller.java
@@ -5,63 +5,93 @@ import java.util.Scanner;
 public class Taller {
 
 	public static void main(String[] args) {
+		boolean isCloseMenu = false;
+		boolean isRepeatEjercicio = true;
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Bienvenido al TALLER DE ALGORITMOS");
 		
+		while (isCloseMenu == false) {
+			showMenu();
+			
+			System.out.println("digite una opcion");
+			int option = sc.nextInt();
+			Puntos puntos = new Puntos();
+			
+			System.out.println(option);
+			
+			switch (option) {
+				case 1: {
+					puntos.p1_promedio(sc);
+					break;
+				}
+				case 2: {
+					puntos.p2_perimetro_cuadrado(sc);
+					break;
+				}
+				case 3: {
+					puntos.p3();
+					break;
+				}
+				case 4: {
+					puntos.p4_perimetro_triangulo(sc);
+					break;
+				}
+				case 5: {
+					puntos.p5_divisores(sc);
+					break;
+				}
+				case 6: {
+					puntos.p6_acumulado(sc);
+					break;
+				}
+				case 7: {
+					puntos.p7_logaritmo();
+					break;
+				}
+				case 8: {
+					puntos.p8_time();
+					break;
+				}
+				case 9: {
+					puntos.p9_suma_cubos(sc);
+					break;
+				}
+				case 10: {
+					puntos.p10_productos(sc, isRepeatEjercicio);
+					break;
+				}
+				default:
+					System.out.println("Opcion invalida");
+					break;
+			}
+			
+			System.out.println("\n");
+			System.out.println("""
+					Terminar ejecucion?
+					0 si 1 no
+					""");
+			if (sc.nextInt() == 0) {
+				isCloseMenu = true;
+			}
+		}
+	}
+	
+	public static void showMenu() {
 		System.out.println("Por favor seleccione un punto del taller");
-		System.out.println("1.");
-		System.out.println("2.");
-		System.out.println("3.");
-		System.out.println("4.");
-		System.out.println("5.");
-		System.out.println("6.");
-		System.out.println("7.");
-		System.out.println("8.");
-		System.out.println("9.");
-		System.out.println("10.");
-		
-		int option = sc.nextInt();
-		
-		Puntos puntos = new Puntos();
-		
-		System.out.println(option);
-		
-		switch (option) {
-		case 1: {
-			puntos.p1_promedio(sc);
-		}
-		case 2: {
-			
-		}
-		case 3: {
-			
-		}
-		case 4: {
-			
-		}
-		case 5: {
-			puntos.p5_divisores(sc);
-		}
-		case 6: {
-			
-		}
-		case 7: {
-			
-		}
-		case 8: {
-			
-		}
-		case 9: {
-			
-		}
-		case 10: {
-			
-		}
-		default:
-			break;
-		}
-		
-		
+		System.out.println("""
+			1.
+			2.
+			3.
+			4.
+			5.
+			6.
+			7.
+			8.
+			9.
+			10.
+		""");
+
 
 	}
 
